aba48724ff670cbc13849b7c7d818fef007caa03
Jamie Cole <jcole05@syr.edu>
2024-09-29 22:49:28 -0400

    completed file 2

--- code/unibrow.py
+++ code/unibrow.py
@@ -5,8 +5,31 @@ import pandas as pd
 import streamlit as st
 import pandaslib as pl
 
-st.title("UniBrow")
-st.caption("The Universal data browser")
+st.title("unibrow")
+st.caption("the universal data browser")
 
-# TODO Write code here to complete the unibrow.py
+file = st.file_uploader("upload a file:", type=["csv", "xlsx", "json"])
+
+if file:
+    file_type = pl.get_file_extension(file.name)
+    df = pl.load_file(file, file_type)
+    columns = pl.get_column_names(df)
+    
+    selected_columns = st.multiselect("select columns to display:", columns, default = columns)
+    
+    if st.toggle("enable filtering"):
+        stcols = st.columns(3)
+        text_columns = pl.get_columns_of_type(df, 'object')
+        filter_column = stcols[0].selectbox("select column to filter:", text_columns) 
+        if filter_column:
+            unique_values = pl.get_unique_values(df, filter_column)
+            selected_value = stcols[1].selectbox("select value to filter by:", unique_values)
+            df_filtered = df[df[filter_column] == selected_value][selected_columns]
+        else:
+            df_filtered = df[selected_columns]
+    else:
+        df_filtered = df[selected_columns]
+    
+    st.dataframe(df_filtered)
+    st.dataframe(df_filtered.describe())
 
